f86881d5599bacbde3a0def8dbe576f737b2f1d5
tsukushibito <yusuke@yusuke-mac.local>
2016-10-07 01:10:35 +0900

    Transformのインターフェース作成途中

--- source/temp/math/transform.h
+++ source/temp/math/transform.h
@@ -16,19 +16,44 @@
 namespace temp {
 namespace math {
 
-template<typename T>
+template < typename T >
 class TransformBase {
 public:
-    TransformBase() : TransformBase(Vec3::kZero, Quat::kIdentity, Vec3(1, 1, 1)) {}
-private:
-    using Vec3 = Vector3Base<T>;
-    using Quat = QuaternionBase<T>;
+    using Vec3 = Vector3Base< T >;
+    using Quat = QuaternionBase< T >;
+    using Mat44 = Matrix44Base< T >;
 
-    Vec3 translation_;
+    TransformBase();
+    TransformBase(const Vec3 &pos, const Quat &rot, const Vec3 &scale);
+
+    TransformBase(const TransformBase &) = default;
+    TransformBase &operator=(const TransformBase &) = default;
+
+#if _MSC_VER<1900
+#else
+    TransformBase(TransformBase &&) noexcept = default;
+    TransformBase &operator=(TransformBase &&) noexcept = default;
+#endif
+
+    ~TransformBase() = default;
+
+    String toString() const;
+
+    Mat44 toMatrix() const;
+
+    TransformBase operator*(const TransformBase &rhs) const;
+    TransformBase combine(const TransformBase &rhs) const;
+
+    const Vec3 &position() const;
+    const Quat &rotation() const;
+    const Vec3 &scale() const;
+
+private:
+    Vec3 position_;
     Quat rotation_;
     Vec3 scale_;
 };
-    
+
 } // namespace math
 } // namespace temp
 
