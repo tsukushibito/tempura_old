a99ec111c1bd5c4b10b5c6dc581d9bcf83c5b903
tsukushibito <yusuke@yusuke-mac.local>
2017-07-09 07:32:50 +0900

    インクルードガードを追加

--- source/temp/resource/resource_common.h
+++ source/temp/resource/resource_common.h
@@ -1,23 +1,30 @@
-#ifndef TMEP_GUARD
-#define TEMP_GUARD
+/**
+ * @file resource_common.h
+ * @brief 
+ * @author tsukushibito
+ * @version 0.0.1
+ * @date 2017-07-09
+ */
+#pragma once
+#ifndef GUARD_b9182cb820a84ff5967c57e597f641e1
+#define GUARD_b9182cb820a84ff5967c57e597f641e1
 
 namespace temp {
 namespace resource {
 
 struct VertexData {
-	graphics::VertexAttribute attribute;
-	graphics::VertexBufferFormat format;
-	ByteData byte_data;
+    graphics::VertexAttribute    attribute;
+    graphics::VertexBufferFormat format;
+    ByteData                     byte_data;
 };
-using VertexDataTable = HashMap<graphics::VertexAttribute, VertexData, graphics::VertexAttributeHash>;
+using VertexDataTable = HashMap<graphics::VertexAttribute, VertexData,
+                                graphics::VertexAttributeHash>;
 
 struct IndexData {
-	graphics::IndexBufferFormat format;
-	graphics::PrimitiveType primitive_type;
-	ByteData byte_data;
+    graphics::IndexBufferFormat format;
+    graphics::PrimitiveType     primitive_type;
+    ByteData                    byte_data;
 };
-
 }
 }
-
-#endif
+#endif // GUARD_b9182cb820a84ff5967c57e597f641e1
