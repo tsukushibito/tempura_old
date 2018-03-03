5055b53a16071a449a3a79c5bbe5cd76e4cbbb4d
tsukushibito <yusuke@yusuke-mac.local>
2018-03-03 18:01:48 +0900

    Fix temp::create()

--- source/temp.cpp
+++ source/temp.cpp
@@ -6,15 +6,35 @@
 #elif defined(TEMP_PLATFORM_MAC)
 #include "temp/app/mac/mac_application.h"
 #endif
+#ifdef TEMP_GRAPHICS_OPENGL
+#include "temp/graphics/opengl/opengl_device.h"
+#else
+#endif
 
 namespace temp {
 
 class TempuraImpl : public Tempura {
  public:
+  TempuraImpl(void* window_handle) {
+    render_thread_ = core::ThreadPool::makeShared("Render", 1);
+    load_thread_ = core::ThreadPool::makeShared("Load", 1);
+#ifdef TEMP_GRAPHICS_OPENGL
+    graphics_device_ = graphics::opengl::OpenGLDevice::create(
+        window_handle, render_thread_, load_thread_);
+#else
+#endif
+  }
   void update(){/*core::Logger::trace("Tempura", "update");*/};
+
+ private:
+  core::ThreadPool::SPtr render_thread_;
+  core::ThreadPool::SPtr load_thread_;
+  graphics::Device::SPtr graphics_device_;
 };
 
-TEMP_DECLSPEC TempSPtr create() { return std::make_shared<TempuraImpl>(); }
+TEMP_DECLSPEC TempSPtr create(void* window_handle) {
+  return std::make_shared<TempuraImpl>(window_handle);
+}
 
 TEMP_DECLSPEC temp::app::ApplicationSPtr createApplication() {
 #if defined(TEMP_PLATFORM_WINDOWS)
