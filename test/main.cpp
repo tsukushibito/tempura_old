cdd7f2bcadc1bb6746bae0bb5b888ef2f368049a
tsukushibito <yusuke@yusuke-mac.local>
2018-03-04 17:36:17 +0900

    Add googletest to project

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,16 @@
-﻿#include <temp.h>
+#include <gtest/gtest.h>
+#include <temp.h>
 
-int main() {
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(ApplicationTest, Run) {
   using namespace temp;
   auto app = createApplication();
   auto engine = create(app->getNativeWindowHandle());
   app->setOnUpdateCallback([&engine]() { engine->update(); });
   auto exit_code = app->run();
-  return exit_code;
+  EXPECT_EQ(exit_code, 0);
 }
