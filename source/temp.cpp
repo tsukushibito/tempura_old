ec2a905a86a6c88b3c960f70ad1ab57ae6072da2
tsukushibito <yusuke@yusuke-mac.local>
2018-03-05 13:34:32 +0900

    Fix TempuraImpl

--- source/temp.cpp
+++ source/temp.cpp
@@ -20,7 +20,8 @@ class TempuraImpl : public Tempura {
     load_thread_ = core::ThreadPool::makeShared("Load", 1);
 #ifdef TEMP_GRAPHICS_OPENGL
     graphics_device_ = graphics::opengl::OpenGLDevice::create(
-        static_cast<HWND>(window_handle), render_thread_, load_thread_);
+        static_cast<graphics::WindowHandle>(window_handle), render_thread_,
+        load_thread_);
 #else
 #endif
   }
