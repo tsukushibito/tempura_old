2ab103875000fe19cb7d556b1d50d3d345eaaaa4
tsukushibito <tsukushibito@gmail.com>
2018-03-07 11:41:53 +0900

    Add quaternion rotation

--- source/temp/math/quaternion.h
+++ source/temp/math/quaternion.h
@@ -15,10 +15,23 @@ class Quaternion {
   Quaternion(const Quaternion&) = default;
   Quaternion& operator=(const Quaternion&) = default;
 
+  Quaternion& operator*=(const Quaternion& other) {
+    auto&& q = *this;
+    auto&& p = other;
+    *this = Quaternion(                                                 //
+        q.x() * p.w() + q.w() * p.x() - q.z() * p.y() + q.y() * p.z(),  //
+        q.y() * p.w() + q.z() * p.x() + q.w() * p.y() - q.x() * p.z(),  //
+        q.z() * p.w() - q.y() * p.x() + q.x() * p.y() + q.w() * p.z(),  //
+        q.w() * p.w() - q.x() * p.x() - q.y() * p.y() - q.z() * p.z()   //
+    );
+    return *this;
+  }
+
   Vector3 eulerAngles() const {
     Vector3 angles;
     auto mat = toMatrix();
     const Float32 threshold = 0.000001f;
+
     if (std::abs(mat(1, 2) + 1.0f) < threshold) {  // sin(x) == 1
       angles.x() = kPi_2;
       angles.y() = 0.0f;
@@ -32,6 +45,10 @@ class Quaternion {
       angles.y() = std::atan2(mat(2, 2), mat(0, 2));
       angles.z() = std::atan2(mat(1, 1), mat(1, 0));
     }
+
+    angles.x() = radianToDegree(2 * angles.x());
+    angles.y() = radianToDegree(2 * angles.y());
+    angles.z() = radianToDegree(2 * angles.z());
     return angles;
   }
 
@@ -42,35 +59,70 @@ class Quaternion {
     return result;
   }
   void normalize() { element_.normalize(); }
-  Quaternion conjugate() const {
-    return Quaternion(-element_.x(), -element_.y(), -element_.z(),
-                      element_.w());
-  }
+  Quaternion conjugate() const { return Quaternion(-x(), -y(), -z(), w()); }
 
   Matrix4x4 toMatrix() const {
-    auto&& e = element_;
+    auto c = conjugate();
     return Matrix4x4(                               //
-        1 - 2 * e.y() * e.y() - 2 * e.z() * e.z(),  // 00
-        2 * e.x() * e.y() + 2 * e.w() * e.z(),      // 01
-        2 * e.x() * e.z() - 2 * e.w() * e.y(),      // 02
+        1 - 2 * c.y() * c.y() - 2 * c.z() * c.z(),  // 00
+        2 * c.x() * c.y() + 2 * c.w() * c.z(),      // 01
+        2 * c.x() * c.z() - 2 * c.w() * c.y(),      // 02
         0,                                          // 03
-        2 * e.x() * e.y() - 2 * e.w() * e.z(),      // 10
-        1 - 2 * e.x() * e.x() - 2 * e.z() * e.z(),  // 11
-        2 * e.y() * e.z() + 2 * e.w() * e.x(),      // 12
+        2 * c.x() * c.y() - 2 * c.w() * c.z(),      // 10
+        1 - 2 * c.x() * c.x() - 2 * c.z() * c.z(),  // 11
+        2 * c.y() * c.z() + 2 * c.w() * c.x(),      // 12
         0,                                          // 13
-        2 * e.x() * e.z() + 2 * e.w() * e.y(),      // 20
-        2 * e.y() * e.z() - 2 * e.w() * e.x(),      // 21
-        1 - 2 * e.x() * e.x() - 2 * e.y() * e.y(),  // 22
+        2 * c.x() * c.z() + 2 * c.w() * c.y(),      // 20
+        2 * c.y() * c.z() - 2 * c.w() * c.x(),      // 21
+        1 - 2 * c.x() * c.x() - 2 * c.y() * c.y(),  // 22
         0,                                          // 23
         0, 0, 0, 1);
   }
+
+  Float32& x() { return element_.x(); }
+  Float32& y() { return element_.y(); }
+  Float32& z() { return element_.z(); }
+  Float32& w() { return element_.w(); }
+  const Float32& x() const { return element_.x(); }
+  const Float32& y() const { return element_.y(); }
+  const Float32& z() const { return element_.z(); }
+  const Float32& w() const { return element_.w(); }
 
   static inline Quaternion identity() {
     return Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
   }
 
+  static inline Quaternion fromXAxisAngle(Float32 deg) {
+    auto rad = degreeToRadian(deg);
+    auto angle = 0.5f * rad;
+    return Quaternion(std::sin(angle), 0.0f, 0.0f, std::cos(angle));
+  }
+
+  static inline Quaternion fromYAxisAngle(Float32 deg) {
+    auto rad = degreeToRadian(deg);
+    auto angle = 0.5f * rad;
+    return Quaternion(0.0f, std::sin(angle), 0.0f, std::cos(angle));
+  }
+
+  static inline Quaternion fromZAxisAngle(Float32 deg) {
+    auto rad = degreeToRadian(deg);
+    auto angle = 0.5f * rad;
+    return Quaternion(0.0f, 0.0f, std::sin(angle), std::cos(angle));
+  }
+
  private:
   Vector4 element_;
 };
+
+inline Quaternion operator*(const Quaternion& lhs, const Quaternion& rhs) {
+  auto result = lhs;
+  return result *= rhs;
+}
+
+inline Vector3 operator*(const Quaternion& lhs, const Vector3& rhs) {
+  auto pos = Quaternion(rhs.x(), rhs.y(), rhs.z(), 0.0f);
+  auto r = lhs * pos * lhs.conjugate();
+  return Vector3(r.x(), r.y(), r.z());
+}
 }  // namespace math
 }  // namespace temp
--- test/quaternon_test.cpp
+++ test/quaternon_test.cpp
@@ -8,8 +8,26 @@ class QuaternionTest : public testing::Test {
 
 TEST_F(QuaternionTest, Angles) {
   using namespace temp::math;
-  auto id = Quaternion::identity();
-  auto mat = id.toMatrix();
-  auto angles = id.eulerAngles();
-  EXPECT_TRUE(angles == Vector3::zero());
+  // auto matZ90 = Matrix4x4(0, -1, 0, 0,  //
+  //                         1, 0, 0, 0,   //
+  //                         0, 0, 1, 0,   //
+  //                         0, 0, 0, 1);
+  auto quatX90 = Quaternion::fromXAxisAngle(90);
+  auto quatY90 = Quaternion::fromYAxisAngle(90);
+  auto quatZ90 = Quaternion::fromZAxisAngle(90);
+  auto quatX180 = Quaternion::fromXAxisAngle(180);
+  auto quatY180 = Quaternion::fromYAxisAngle(180);
+  auto quatZ180 = Quaternion::fromZAxisAngle(180);
+  auto matX90 = quatX90.toMatrix();
+  auto matY90 = quatY90.toMatrix();
+  auto matZ90 = quatZ90.toMatrix();
+  auto right = Vector3::right();
+  auto up = quatZ90 * right;
+  auto forward = quatX90 * up;
+  auto right_ = quatY90 * forward;
+  auto right4 = Vector4(right);
+  auto up4 = matZ90 * right4;
+  auto forward4 = matX90 * up4;
+  auto right4_ = matY90 * forward4;
+  EXPECT_TRUE(right == right_);
 }
