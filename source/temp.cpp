a0d00304d9a8e891fae300a7f51bae4aaf54a6a2
tsukushibito <yusuke@yusuke-mac.local>
2018-03-19 23:04:03 +0900

    Revert to shared lib engine

--- source/temp.cpp
+++ source/temp.cpp
@@ -26,6 +26,76 @@ class TempuraImpl : public Tempura {
 #endif
     resource_manager_ =
         resource::ResourceManager::makeShared(load_thread_, graphics_device_);
+
+    TEMP_LOG_INFO("Tempra", filesystem::current_path().string());
+
+    auto mesh = resource_manager_->create<resource::Mesh>("test.tmsh");
+    mesh->load(true);
+#if 0
+    auto graphics_device = engine->graphicsDevice();
+    Float32 vertices[] = {
+        -1.0f, -1.0f, -1.0f, 0.0f,  //
+        0.0f,  0.0f,  -1.0f, 0.0f,  //
+        -1.0f, 1.0f,  -1.0f, 0.0f,  //
+        0.0f,  0.0f,  -1.0f, 0.0f,  //
+        1.0f,  -1.0f, -1.0f, 0.0f,  //
+        0.0f,  0.0f,  -1.0f, 0.0f,  //
+        1.0f,  1.0f,  -1.0f, 0.0f,  //
+        0.0f,  0.0f,  -1.0f, 0.0f,  //
+
+        1.0f,  -1.0f, -1.0f, 0.0f,  //
+        1.0f,  0.0f,  0.0f,  0.0f,  //
+        1.0f,  1.0f,  -1.0f, 0.0f,  //
+        1.0f,  0.0f,  0.0f,  0.0f,  //
+        1.0f,  -1.0f, 1.0f,  0.0f,  //
+        1.0f,  0.0f,  0.0f,  0.0f,  //
+        1.0f,  1.0f,  1.0f,  0.0f,  //
+        1.0f,  0.0f,  0.0f,  0.0f,  //
+
+        1.0f,  -1.0f, 1.0f,  0.0f,  //
+        0.0f,  0.0f,  1.0f,  0.0f,  //
+        1.0f,  1.0f,  1.0f,  0.0f,  //
+        0.0f,  0.0f,  1.0f,  0.0f,  //
+        -1.0f, -1.0f, 1.0f,  0.0f,  //
+        0.0f,  0.0f,  1.0f,  0.0f,  //
+        -1.0f, 1.0f,  1.0f,  0.0f,  //
+        0.0f,  0.0f,  1.0f,  0.0f,  //
+
+        -1.0f, -1.0f, 1.0f,  0.0f,  //
+        -1.0f, 0.0f,  0.0f,  0.0f,  //
+        -1.0f, 1.0f,  1.0f,  0.0f,  //
+        -1.0f, 0.0f,  0.0f,  0.0f,  //
+        -1.0f, -1.0f, -1.0f, 0.0f,  //
+        -1.0f, 0.0f,  0.0f,  0.0f,  //
+        -1.0f, 1.0f,  -1.0f, 0.0f,  //
+        -1.0f, 0.0f,  0.0f,  0.0f,  //
+    };
+    graphics::VertexBufferDesc vb_desc;
+    vb_desc.element_count = 2;
+    vb_desc.element_descs[0].format = graphics::VertexFormat::kFloat32x4;
+    vb_desc.element_descs[0].attribute = graphics::VertexAttribute::kPosition;
+    vb_desc.element_descs[1].format = graphics::VertexFormat::kFloat32x4;
+    vb_desc.element_descs[1].attribute = graphics::VertexAttribute::kNormal;
+    vb_desc.size = sizeof(vertices);
+    auto vb = graphics_device->createVertexBuffer(vb_desc, vertices);
+    mesh->replaceVertexBuffer(vb);
+    Float32 indices[] = {
+        0, 1, 2, 3,  //
+        3, 2,        //
+        2, 3, 4, 5,  //
+        5, 4,        //
+        4, 5, 6, 7,  //
+        7, 6,        //
+        6, 7, 0, 1,  //
+    };
+    graphics::IndexBufferDesc ib_desc;
+    ib_desc.format = graphics::IndexBufferFormat::kUInt16;
+    ib_desc.primitive_type = graphics::PrimitiveType::kTriangleStrip;
+    ib_desc.size = sizeof(indices);
+    auto ib = graphics_device->createIndexBuffer(ib_desc, indices);
+    mesh->replaceIndexBuffer(ib);
+    mesh->save();
+#endif
   }
   void update(){/*core::Logger::trace("Tempura", "update");*/};
 
@@ -43,11 +113,11 @@ class TempuraImpl : public Tempura {
   renderer::Renderer::SPtr renderer_;
 };
 
-TempSPtr create(void* window_handle) {
+TEMP_DECLSPEC TempSPtr create(void* window_handle) {
   return std::make_shared<TempuraImpl>(window_handle);
 }
 
-temp::app::ApplicationSPtr createApplication() {
+TEMP_DECLSPEC temp::app::ApplicationSPtr createApplication() {
 #if defined(TEMP_PLATFORM_WINDOWS)
   return std::make_shared<temp::app::windows::WindowsApplication>();
 #elif defined(TEMP_PLATFORM_MAC)
